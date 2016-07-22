bbb09515afcc15c16dc38a6d4929ac640d8580b9
bmovick <bmovick@gmail.com>
2016-07-22 02:43:15 -0500

    RTC 434321 - Update UI to be consistent

--- www/js/takeapic.js
+++ www/js/takeapic.js
@@ -101,3 +101,28 @@ function callGoogleVision(data){
         });
     }
 
+function startDictation() {
+
+    if (window.hasOwnProperty('webkitSpeechRecognition')) {
+
+        var recognition = new webkitSpeechRecognition();
+
+        recognition.continuous = false;
+        recognition.interimResults = false;
+
+        recognition.lang = "en-US";
+        recognition.start();
+
+        recognition.onresult = function(e) {
+            document.getElementById('transcript').value
+                                                             = e.results[0][0].transcript;
+            recognition.stop();
+            document.getElementById('labnol').submit();
+        };
+
+        recognition.onerror = function(e) {
+            recognition.stop();
+        }
+
+    }
+}
